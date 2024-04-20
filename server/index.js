9e9ba0476810cd54ead6866394330cd8a7c65a04
Edison <solisedison@outlook.com>
2024-04-20 12:06:28 -0500

    add-morgan dependency and configurated the port

---
+++ server/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import logger from 'morgan';
+const port = process.env.PORT ?? 3000;
+const app = express();
+app.use(logger('dev'))
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+})
