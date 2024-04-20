1236ef0ffa50079fa5d642dabd7421e4b96ba61f
Edison <solisedison@outlook.com>
2024-04-20 12:25:06 -0500

    configuration a server with socket.io

--- server/index.js
+++ server/index.js
@@ -1,13 +1,21 @@
 import express from 'express';
 import logger from 'morgan';
+import {Server} from 'socket.io';
+import {createServer} from 'node:http';
 const port = process.env.PORT ?? 3000;
 const app = express();
+const server= createServer(app);
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+})
 app.use(logger('dev'))
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.sendFile(process.cwd() + '/cliente/index.html');
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 })
