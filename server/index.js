350c8ce61dd0c6c3f1cdff9851835cf422b9c9b5
Edison <solisedison@outlook.com>
2024-04-20 12:49:05 -0500

    finished the server without the DB

--- server/index.js
+++ server/index.js
@@ -2,10 +2,14 @@ import express from 'express';
 import logger from 'morgan';
 import {Server} from 'socket.io';
 import {createServer} from 'node:http';
+import dotenv from 'dotenv';
+//aqui crear el cliente de sqlite de turso 
 const port = process.env.PORT ?? 3000;
 const app = express();
 const server= createServer(app);
-const io = new Server(server);
+const io = new Server(server,{
+    connectionStateRecovery: {},
+});
 
 io.on('connection', (socket) => {
     console.log('a user connected');
