e5e4d88646d1cb834c26853d4f6f32af92343b4f
Edison <solisedison@outlook.com>
2024-04-20 12:34:56 -0500

    created the broadcast

--- server/index.js
+++ server/index.js
@@ -9,7 +9,16 @@ const io = new Server(server);
 
 io.on('connection', (socket) => {
     console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });  
+    socket.on('chat message', (msg) => {
+        //console.log('message: ' + msg);
+        //broadCast para emitir un mensaje a todos los clientes
+        io.emit('chat message', msg);
+    }); 
 })
+
 app.use(logger('dev'))
 
 app.get('/', (req, res) => {
